cdce42591444699a2b8ff647298ed61a5a79a79b
Costa A <cgaspro2@gmail.com>
2021-08-31 15:40:11 +1000

    Initial commit to github

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
-export function getHeadlines() {
-    const url = 'http://131.181.190.87:3000/rankings?year=2020';
+  function getYear(year) {
+    const url = 'http://131.181.190.87:3000/rankings?year='+year;
     return fetch(url)
     .then((res) => res.json())
     .then((res) =>
@@ -13,31 +13,144 @@ export function getHeadlines() {
             year: res.year,
         })),
     )
-
 }
 
-export function useNewsArticles(){
+export function getYearReg(year) {
+    const url = 'http://131.181.190.87:3000/factors/'+year;
+    let token = localStorage.getItem("token");
+    return fetch(url, {
+        headers: {accept: "application/json", Authorization: "Bearer " + token},
+    })
+    .then((res) => res.json())
+    .then((res) =>
+        // get just the title and url from each article reg
+        res.map((res) => ({
+            rank: res.rank,
+            country: res.country,
+            score: res.score,
+            economy: res.economy,
+            family: res.family,
+            health: res.health,
+            freedom: res.freedom,
+            generosity: res.generosity,
+            trust: res.trust,
+        })),
+    )
+}
+
+function getCountryRank(Country) {
+    const url = 'http://131.181.190.87:3000/rankings?country='+Country;
+    return fetch(url)
+    .then((res) => res.json())
+    .then((res) =>
+        // get just the title and url from each article
+        res.map((res) => ({
+            year: res.year,
+            score: res.score,
+            rank: res.rank,
+        })),
+    )
+}
+
+function getCountryList() {
+    const url = 'http://131.181.190.87:3000/countries';
+    return fetch(url)
+    .then((res) => res.json())
+}
+
+
+export function useCountries(year, country){
+    
     const [loading, setLoading] = useState(true);
-    const [headlines, setHeadlines] = useState(true);
+    const [countires, setCountries] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        if(year == null){
+                getCountryRank(country).then((countires) => {
+                setCountries(countires);
+                setLoading(false);
+                }).catch((e) => {
+                    setError(e);
+                    setLoading(false);
+                })
+        }else{
+                getYear(year).then((countires) => {
+                setCountries(countires);
+                setLoading(false);
+                }).catch((e) => {
+                    setError(e);
+                    setLoading(false);
+                })
+        }
         
-        getHeadlines().then((headlines) => {
-        setHeadlines(headlines);
-        setLoading(false);
-        }).catch((e) => {
-            setError(e);
-            setLoading(false);
-        })
         
     }, []);
 
 
     return {
         loading,
-        headlines,
+        countires,
         error,
     }
+}
+
+export function useCountriesReg(year, country){
+    
+    const [loading, setLoading] = useState(true);
+    const [countires, setCountries] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if(year == null){
+                getCountryRank(country).then((countires) => {
+                setCountries(countires);
+                setLoading(false);
+                }).catch((e) => {
+                    setError(e);
+                    setLoading(false);
+                })
+        }else{
+                getYearReg(year).then((countires) => {
+                setCountries(countires);
+                setLoading(false);
+                }).catch((e) => {
+                    setError(e);
+                    setLoading(false);
+                })
+        }
+        
+        
+    }, []);
+
+
+    return {
+        loading,
+        countires,
+        error,
+    }
+}
+
+export function useCountriesList(){
+    const [loading, setLoading] = useState(true);
+    const [countires, setCountries] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+                getCountryList().then((countires) => {
+                setCountries(countires);
+                setLoading(false);
+                }).catch((e) => {
+                    setError(e);
+                    setLoading(false);
+                })
+    }, []);
+
+    return {
+        loading,
+        countires,
+        error,
+    }
+
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Country from "./Country";
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import Country from "./components/Country";
 import styles from "./style.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import {YearApiTable, YearApiTableReg} from "./components/table.js";
+import { LoginPage} from "./components/login.js"
+import {RegisterPage} from "./components/register.js";
+import {Navbar} from "./components/navbar.js";
 
-import {ApiTable} from "./table.js";
+
+let token = localStorage.getItem("token");
+let email = localStorage.getItem("email");
+
 
   function App() {
+  
+    const [year, setYear] = useState(2020);
+    console.log(token);
+    console.log(email);
+    
+    function DisplayTable(props){
+      var year = props.year;
+      console.log(year)
+        return(
+          <div>
+            
    
+   {(token!=null && token.length > 0) &&
+   <YearApiTableReg year={year}/>
+    }
+
+    {(token==null || token ==undefined) &&
+    <YearApiTable year={year}/>
+    }
+          </div>
+          
+        )
+    }
+
     return (
-    <div> api table assignments
+    <div> 
       <div className={styles.Container}>
+
+      
+    <Navbar/>
         <div className={styles.Center}>
-          <ApiTable/>
+        Showing results for {year} <br></br>
+         <input type="text" id="searchtext"></input>
+
+<select id="search" name="search">
+    <option value="year">Year</option>
+   
+</select>
+
+<Button
+color="info"
+size="sm"
+className="mt-3"
+onClick={() => {console.log(document.getElementById("search").value); 
+           setYear(document.getElementById("searchtext").value);
+           }}
+>
+Search
+</Button>
+            
+            {/* <YearApiTable year={year}/> */}
+
+           <DisplayTable year={year}/>
+          
+           
+        
         </div> 
       </div>
     </div>
@@ -26,6 +85,12 @@ ReactDOM.render(
       <switch>
         <Route path="/country">
           <Country/>
+        </Route>
+        <Route path="/login">
+          <LoginPage/>
+        </Route>
+        <Route path="/register">
+          <RegisterPage/>
         </Route>
         <Route exact path="/">
           <App/>
